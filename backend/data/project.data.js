3f2a15096fe80f2a10b60c6bfcae23ffce7a8e04
jmndao <jmndao@gmail.com>
2021-07-31 19:01:57 +0000

    Adding website(Link) to miashop project in project.data.js

--- backend/data/project.data.js
+++ backend/data/project.data.js
@@ -17,6 +17,7 @@ const projects = [{
         description: "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
         private: true,
         doneAt: new Date(2021, 11, 17, 3, 24, 0), // or Date('1995-12-17T03:24:00')
+        website: "https://jmndao.github.io/miashop/",
         forkLink: "https://github.com/jmndao/miashop.git",
         github: "https://github.com/jmndao/miashop"
     }
