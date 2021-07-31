2e9433aba9b6db47f1ddd6821c61d219b241b42a
jmndao <jmndao@gmail.com>
2021-07-31 19:36:29 +0000

    changing image dir assets->static

--- backend/data/project.data.js
+++ backend/data/project.data.js
@@ -1,6 +1,6 @@
 const projects = [{
         name: "Easy Money",
-        image: "/assets/images/easymoney.png",
+        image: "/static/images/easymoney.png",
         description: "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
         private: true,
         doneAt: new Date(2021, 11, 17, 3, 24, 0), // or Date('1995-12-17T03:24:00')
@@ -8,12 +8,12 @@ const projects = [{
         participant: [{
             name: "Akhad Top",
             pseudo: "Top",
-            image: "/assets/images/top.png"
+            image: "/static/images/top.png"
         }]
     },
     {
         name: "Easy Money",
-        image: "/assets/images/miashop.png",
+        image: "/static/images/miashop.png",
         description: "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
         private: true,
         doneAt: new Date(2021, 11, 17, 3, 24, 0), // or Date('1995-12-17T03:24:00')
