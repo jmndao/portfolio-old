ce85a75882fcfa6c9aa71c35f45caba0bd38213a
jmndao <jmndao@gmail.com>
2021-08-03 11:20:15 +0000

    About data updated

--- backend/data/about.data.js
+++ backend/data/about.data.js
@@ -6,97 +6,99 @@ const about = {
             school: "DAUST",
             fromYear: "2019",
             toYear: "Current",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati temporibus impedit beatae porro, dolorum ut. Repellendus consequuntur assumenda obcaecati reiciendis aperiam natus. Ea explicabo perferendis cumque corrupti, saepe libero aperiam?"
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati temporibus impedit beatae porro, dolorum ut. Repellendus consequuntur assumenda obcaecati reiciendis aperiam natus. Ea explicabo perferendis cumque corrupti, saepe libero aperiam?",
         },
         {
             school: "EC2LT",
             fromYear: "2017",
             toYear: "2019",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati temporibus impedit beatae porro, dolorum ut. Repellendus consequuntur assumenda obcaecati reiciendis aperiam natus. Ea explicabo perferendis cumque corrupti, saepe libero aperiam?"
-        }
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati temporibus impedit beatae porro, dolorum ut. Repellendus consequuntur assumenda obcaecati reiciendis aperiam natus. Ea explicabo perferendis cumque corrupti, saepe libero aperiam?",
+        },
     ],
     programmingSkill: [{
             language: "React JS",
             image: "/static/images/react.svg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
         },
         {
             language: "Node JS",
             image: "/static/images/nodejs.svg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
         },
         {
             language: "Django",
             image: "/static/images/django.svg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
         },
         {
             language: "HTML 5",
             image: "/static/images/html5.svg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
         },
         {
             language: "CSS 3",
             image: "/static/images/css3.svg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
         },
         {
             language: "Python",
             image: "/static/images/python.svg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-        }
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        },
     ],
     languageSkill: [{
             flag: "/static/images/usa.svg",
             level: "Fluent",
-            levelRate: 4.5
+            levelRate: 4.5,
         },
         {
             flag: "/static/images/france.svg",
             level: "Fluent",
-            levelRate: 4
+            levelRate: 4,
         },
         {
             flag: "/static/images/arabe.svg",
             level: "Debutant",
-            levelRate: 2
+            levelRate: 2,
         },
     ],
     otherSkill: [
         { name: "Firebase", image: "/static/images/firebase.svg" },
         { name: "Heroku", image: "/static/images/heroku.svg" },
         { name: "cPanel", image: "/static/images/cpanel.svg" },
+        { name: "Postman", image: "/static/images/postman.svg" },
+        { name: "RESTful API", image: "/static/images/restful_api.svg" },
     ],
     workExperience: [{
             enterpriseName: "Giga Electronic",
             position: "Software Developer",
             duration: 6,
-            website: "https://giga-electronic.sn"
+            website: "https://giga-electronic.sn",
         },
         {
             enterpriseName: "Easy Money",
             position: "Software Developer",
             duration: 0,
-            website: "https://www.easymoney.sn"
+            website: "https://www.easymoney.sn",
         },
     ],
     activity: [
         { name: "Swimming" },
         { name: "BasketBall" },
-        { name: "Video Game" }
+        { name: "Video Game" },
     ],
     interest: [
         { name: "Machine Learning" },
         { name: "Data Science" },
         { name: "Cloud Storage" },
-        { name: "BlockChain" }
+        { name: "BlockChain" },
     ],
-    certification: [
-        { name: "Node Master Class" },
-        { image: "/static/images/node_master_class.png" },
-        { link: "https://www.example.com" },
-        { from: "Pirple" }
-    ]
+    certification: [{
+        name: "Node Master Class",
+        image: "/static/images/node_master_class.png",
+        link: "https://www.example.com",
+        from: "Pirple",
+    }, ],
 };
 
 export default about;
