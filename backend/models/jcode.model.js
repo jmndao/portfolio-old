ffc568975307bff9df63ee6658609aa304da75d3
jmndao <jmndao@gmail.com>
2021-08-03 09:32:16 +0000

    JCode Model updated

--- backend/models/jcode.model.js
+++ backend/models/jcode.model.js
@@ -2,14 +2,20 @@ import mongoose from "mongoose";
 
 const jCodeSchema = mongoose.Schema({
 
-    title: { type: String, required: true },
-    mode: { type: String, required: true },
-    text: { type: String, required: true },
-    code: { type: String, required: true },
-    links: [{
-        url: { type: String, required: true },
-        video: { type: Boolean, default: false }
-    }]
+    project: { type: String, required: true },
+    description: { type: String, required: true },
+    section: [{
+        title: { type: String, required: true },
+        mode: { type: String, required: true },
+        text: { type: String, required: true },
+        code: { type: String, required: true },
+        links: [{
+            url: { type: String, required: true },
+            video: { type: Boolean, default: false }
+        }]
+    }],
+    link: { type: String },
+    mention: [{ name: { type: String, required: true } }]
 
 }, { timestamps: true });
 
