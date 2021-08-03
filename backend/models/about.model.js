5f0f9c350736794abf5d4466e2c086caafe67e54
jmndao <jmndao@gmail.com>
2021-08-03 09:08:25 +0000

    About Mode update [otherSkill, interest]

--- backend/models/about.model.js
+++ backend/models/about.model.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-
 const aboutSchema = mongoose.Schema({
     entry: { type: String, required: true },
     about: { type: String, required: true },
@@ -9,30 +8,36 @@ const aboutSchema = mongoose.Schema({
         school: { type: String, required: true },
         fromYear: { type: String },
         toYear: { type: String },
-        description: { type: String, required: true }
-    }],
+        description: { type: String, required: true },
+    }, ],
     programmingSkill: [{
         language: { type: String, required: true },
         image: { type: String, required: true },
-        description: { type: String, required: true }
+        description: { type: String, required: true },
+    }, ],
+    otherSkill: [{
+        name: { type: String, required: true },
+        image: { type: String, required: true },
     }],
     languageSkill: [{
         flag: { type: String, required: true },
         level: { type: String, required: true },
-        levelRate: { type: Number, required: true, default: 2 }
-    }],
+        levelRate: { type: Number, required: true, default: 2 },
+    }, ],
     workExperience: [{
         enterpriseName: { type: String, required: true },
         position: { type: String, required: true },
         duration: { type: Number, default: 0 },
-        website: { type: String, required: true }
-    }],
+        website: { type: String, required: true },
+    }, ],
     activity: [{
-        name: { type: String, required: true }
-    }]
+        name: { type: String, required: true },
+    }, ],
+    interest: [{
+        name: { type: String, required: true },
+    }, ]
 }, { timestamps: true });
 
-
-const About = mongoose.model('About', aboutSchema);
+const About = mongoose.model("About", aboutSchema);
 
 export default About;
