fbf58665fea73af78c4508144f8277494c4c547c
jmndao <jmndao@gmail.com>
2021-08-22 07:47:12 +0000

    AdminProjectEdit

--- frontend/src/Screens/AdminProjectEdit.js
+++ frontend/src/Screens/AdminProjectEdit.js
@@ -1,8 +1,10 @@
-import moment from "moment";
+import moment, { defaultFormat } from "moment";
+import 'moment/locale/zh-cn';
+import locale from 'antd/lib/locale/en_US';
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProject, updateProject } from "../actions/project.action";
-import { Button, Form, Switch, DatePicker, Input, Space, Spin, Upload, message } from "antd";
+import { Button, Form, Switch, DatePicker, Input, Space, Spin, Upload, message, ConfigProvider } from "antd";
 import { LoadingOutlined, MinusCircleOutlined, PlusOutlined, UploadOutlined } from "@ant-design/icons";
 import { firebaseRemove, firebaseUpload } from "../actions/about.action";
 import Admin from "./Admin";
@@ -11,6 +13,7 @@ const AdminProjectEdit = ({ match }) => {
 
     const projectID = match.params.id;
 
+    const dateFormat = 'YYYY-MM-DD'
     const [editForm] = Form.useForm();
     const dispatch = useDispatch();
 
@@ -78,7 +81,12 @@ const AdminProjectEdit = ({ match }) => {
                                             <Switch defaultChecked={true} />
                                         </Form.Item>
                                         <Form.Item label="Done" name="doneAt">
-                                            <Input type='date' />
+                                            <ConfigProvider locale={locale}>
+                                                { project && <DatePicker  
+                                                    defaultValue={moment(project.doneAt, defaultFormat)}
+                                                    format={defaultFormat}
+                                                /> }
+                                            </ConfigProvider>;
                                         </Form.Item>
                                         <Form.Item label="Website Link" name="website">
                                             <Input />
